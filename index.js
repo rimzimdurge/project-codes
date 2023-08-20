278a8b372cf8c4d07451f4fe6820601b1339acc3
rimzimdurge <131154076+rimzimdurge@users.noreply.github.com>
2023-08-20 14:27:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,112 @@
-const express=require("express")
-const app=express()
-const path=require("path")
-const hbs=require("hbs")
-const collection=require("./mongodb")
+var questionBank= [
+    {
+        question : 'What is the capital of France?',
+        option : ['London','Madrid','Paris','Rome'],
+        answer : 'Paris'
+    },
+    {
+        question : ' What is the largest planet in our solar system?',
+        option : ['Venus','Mars','Jupiter','Saturn'],
+        answer : 'Jupiter'
+    },
+    {
+        question : 'Which famous scientist developed the theory of relativity?',
+        option : ['Isaac Newton','Galileo Galilei','Albert Einstein','Marie Curie'],
+        answer : 'Albert Einstein'
+    },
+    {
+        question : 'Which country is known as the "Land of the Rising Sun"?',
+        option : ['China','Japan','India','South Korea'],
+        answer : 'Japan'
+    },
+    {
+        question : 'What is the chemical symbol for the element gold?',
+        option : ['Go','Gd','Au','Ag'],
+        answer : 'Au'
+    },
+    {
+        question : ' Which planet is known as the "Red Planet"?',
+        option : ['Venus','Mars','Jupiter','Mercury'],
+        answer : 'Mars'
+    },
+    {
+        question : ' What is the largest ocean on Earth?',
+        option : [' Atlantic Ocean',' Indian Ocean','Pacific Ocean','Arctic Ocean'],
+        answer : 'Pacific Ocean'
+    }
+]
 
-const templatepath=path.join(__dirname,'../templates')
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
 
-app.use(express.json())
-app.set("view engine","hbs")
-app.set("views",templatepath)
-app.use(express.urlencoded({extended:false}))
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
 
-app.get("/",(req,res)=>{
-    res.render("login")
-})
-
-app.post("/signup",async (req,res)=>{
-const data={
-    name:req.body.name,
-    password:req.body.password
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+''
+    +questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' ' +questionBank.length;
 }
 
-await collection.insertMany([data])
-res.render("home")
-
-})
-
-
-app.get("/signup",(req,res)=>{
-    res.render("signup")
-})
-
-app.post("/login",async (req,res)=>{
-    try{
-       const check=await collection.findOne({name:req.body.name})
-
-       if(check.password===req.body.password){
-        res.render("home") 
-       }
-       else{
-        res.send("wrong password")
-       }
-       
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
     }
-    catch{
-        res.send("wrong details")
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
     }
+    setTimeout(nextQuestion,300);
+}
 
-     })
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none'
+        scoreboard.style.display= 'block'
+    }
+}
 
+//click events to next button
+next.addEventListener('click',nextQuestion);
 
+//back to quiz button event
+function backToQuiz(){
+    location.reload();
+}
 
-
-app.listen(3000,()=>{
-    console.log("port connected");
-})
+//function to check answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+displayQuestion();
